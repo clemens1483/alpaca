c14408d42f7bf417f5c7e25619db29e5d573d4fd
Clemens H <clemens1483@gmail.com>
2019-09-14 21:18:09 +0300

    first working app

--- app.rb
+++ app.rb
@@ -6,7 +6,6 @@ require './models/key'  #load class
 
 
 get "/" do
-
 		#should check array size is small to prevent large arrays for DDOS
 		@melody = params[:melody].upcase.split(",")
 		#shift parameter
@@ -18,24 +17,37 @@ get "/" do
 		#.pluck(:id) only returns id of the keys
 		# .to_a converts to array
 		#Should put this in first if statement for efficiency
-		@parameter_db = Key.where(name: @melody).pluck(:id).to_a
+		#@parameter_db = Key.where(name: @melody).pluck(:id).to_a
+		#initialise empty array
+		@parameter_db = []
+
+		@melody.each do |note|
+			if Key.exists?(name: note)
+			  note_id = Key.where(name: note).pluck(:id).join.to_i
+			  @parameter_db << note_id
+			end
+		end
+
 
 		#converts shift to integer and back to string and checks if it's the same
 		if @shift.to_i.to_s == @shift
 			#should check how to redfine variable value
-			@shift = @shift.to_i
 	        #checks if the table array contains as many keys as the parameters
 	        #in which case all the keys are valid
 	        if @parameter_db.length == @melody.length
 	          #shifts every member of parameter_db by shift
-	          shift_id_arr = @parameter_db.map { |a| a+@shift.to_i }
-	          if shift_id_arr.max <= 88
+	          shift_id_a = @parameter_db.map { |a| a+@shift.to_i }	          
+	          if shift_id_a.max <= 88
 	            #finds corresponding keys in table
-	            shift_name_arr = Key.where(id: shift_id_arr).pluck(:name).to_a
-	            @keys = shift_name_arr
-	          else
-	            @out_of_range = true 
-	          end
+	            #shift_name_a = Key.where(id: shift_id_a).pluck(:name).to_a
+	            shift_name_a = []
+	            shift_id_a.each do |note|
+	              shift_name_a << Key.where(id: note).pluck(:name).join.to_s
+	            end
+	          @keys = shift_name_a
+	         else
+	          @out_of_range = true 
+	         end
 
 
 	        else
@@ -49,13 +61,5 @@ get "/" do
 
 end
 
-#get '/models' do
-#	@models = Key.all
-	#erb :models
-#end
-
-
-
-		
 
 
