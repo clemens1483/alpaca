d75538ff3c5d0d5bfe3cb421be4c12d5e50a909f
Clemens H <clemens1483@gmail.com>
2019-09-14 21:56:26 +0300

    cleaned up app.rb

--- app.rb
+++ app.rb
@@ -6,58 +6,51 @@ require './models/key'  #load class
 
 
 get "/" do
-		#should check array size is small to prevent large arrays for DDOS
-		@melody = params[:melody].upcase.split(",")
-		#shift parameter
-		@shift = params[:shift]
-		
-		
-		#creates an active record relation with all entries that match the name
-		#of the keys in the search array and converts it to an array
-		#.pluck(:id) only returns id of the keys
-		# .to_a converts to array
-		#Should put this in first if statement for efficiency
-		#@parameter_db = Key.where(name: @melody).pluck(:id).to_a
-		#initialise empty array
-		@parameter_db = []
+    #create array of notes in melody parameter
+	@melody = params[:melody].upcase.split(",")
+	#shift parameter
+	@shift = params[:shift]
+	#initialise array for ids of notes
+	notes_id_a = []
+	#checks if each note exists in the database and if it does adds its id to the array notes_id_a
+	@melody.each do |note|
+	  if Key.exists?(name: note)
+	    note_id = Key.where(name: note).pluck(:id).join.to_i
+		notes_id_a << note_id
+	  end
+	end
 
-		@melody.each do |note|
-			if Key.exists?(name: note)
-			  note_id = Key.where(name: note).pluck(:id).join.to_i
-			  @parameter_db << note_id
-			end
-		end
-
-
-		#converts shift to integer and back to string and checks if it's the same
-		if @shift.to_i.to_s == @shift
-			#should check how to redfine variable value
-	        #checks if the table array contains as many keys as the parameters
-	        #in which case all the keys are valid
-	        if @parameter_db.length == @melody.length
-	          #shifts every member of parameter_db by shift
-	          shift_id_a = @parameter_db.map { |a| a+@shift.to_i }	          
-	          if shift_id_a.max <= 88
-	            #finds corresponding keys in table
-	            #shift_name_a = Key.where(id: shift_id_a).pluck(:name).to_a
-	            shift_name_a = []
-	            shift_id_a.each do |note|
-	              shift_name_a << Key.where(id: note).pluck(:name).join.to_s
-	            end
-	          @keys = shift_name_a
-	         else
-	          @out_of_range = true 
-	         end
-
-
-	        else
-	        	@melody_invalid = true
-	        end
+	#checks if @shift has the form of an integer
+	if @shift.to_i.to_s == @shift 
+        #checks if notes_id_a contains as many notes as @melody in which case all the keys are valid
+        if notes_id_a.length == @melody.length
+          #shifts every member of notes_id_a by shift
+          shift_id_a = notes_id_a.map { |a| a+@shift.to_i }
+          #checks if the resulting ids are out of range of the Key table	          
+          if shift_id_a.max <= 88
+          	#initialises array for shifted key names used in /views/melody.erb
+            @shift_name_a = []
+            #finds the name for each key with id in shift_id_a array and adds it to shift_name_a
+            shift_id_a.each do |key|
+              key_name = Key.where(id: key).pluck(:name).join.to_s
+              @shift_name_a << key_name
+            end
+          else
+            #the the ids in shifted array shift_id_a are out of range 
+            @out_of_range = true  #for /views/melody/erb
+          end
 
         else
-        	@shift_invalid = true
-         end
-         erb :melody
+          #the melody contains invalid keys
+          @melody_invalid = true  #for /views/melody/erb
+        end
+
+    else
+      #shift is not an integer
+      @shift_invalid = true  #for /views/melody/erb
+    end
+    #call views/melody.erb
+    erb :melody
 
 end
 
